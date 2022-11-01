8d31219573b5c30cdea5eccd3898145d1e970d0e
C1385 <C1385@oic.osaka.jp>
2022-11-01 17:05:58 +0900

    Titleシーンの結合

--- Project/TitleScene.cpp
+++ Project/TitleScene.cpp
@@ -1,26 +1,28 @@
 #include "TitleScene.h"
 
-void TitleScene::Initialize()
-{
+void TitleScene::Initialize() {
 
 }
 
-void TitleScene::ReLoad()
-{
+void TitleScene::ReLoad() {
 
 }
 
-void TitleScene::Update()
-{
-
+void TitleScene::Update() {
+	InputMouseKey();
 }
 
-void TitleScene::Render()
-{
+void TitleScene::InputMouseKey() {
 
+	if (g_pInput->IsMouseKeyPush(MOFMOUSE_LBUTTON) || g_pInput->IsMouseKeyPush(MOFMOUSE_RBUTTON)) {
+		SceneManager::Instance().ChangeScene(SCENE_TYPE::STAGESELECT);
+	}
 }
 
-void TitleScene::Release()
-{
+void TitleScene::Render() {
+	CGraphicsUtilities::RenderString(30, 30, "TitleScene");
+}
+
+void TitleScene::Release() {
 
 }
--- Project/TitleScene.h
+++ Project/TitleScene.h
@@ -1,12 +1,15 @@
 #pragma once
+#include	"SceneManager.h"
 #include	"IBaseScene.h"
-
+#include    <Mof.h>
 class TitleScene :public IBaseScene
 {
+
 public:
 	void Initialize();
 	void ReLoad();
 	void Update();
+	void InputMouseKey();
 	void Render();
 	void Release();
 };
