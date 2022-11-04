733c2e0564912b071d86b9fe8972ff1c827a5534
DESKTOP-MTBO802\energ <w>
2022-11-04 22:45:14 +0900

    Menuの位置調整

--- Project/ReturnStageSelect.cpp
+++ Project/ReturnStageSelect.cpp
@@ -1,7 +1,7 @@
 #include "ReturnStageSelect.h"
 void ReturnStageSelect::Initialize()
 {
-	_pos = Vector2(200, 500);
+	_pos = Vector2(400, 500);
 	_size = Vector2(200, 120);
 	_button.SetStatu(_pos, _size);
 }
