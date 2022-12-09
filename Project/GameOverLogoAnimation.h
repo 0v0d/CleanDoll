cbead40bbfe5280e1174020b3362965bdcdf17b5
C1307 <C1307@oic.osaka.jp>
2022-12-09 13:55:20 +0900

    失敗のテロップアニメーション追加完了

---
+++ Project/GameOverLogoAnimation.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Mof.h"
+class GameOverLogoAnimation
+{
+private:
+	CSpriteMotionController _animation;
+	CTexture _missTakeLogTexture;
+	Vector2 _pos;
+	CRectangle _missTakeLogRect;
+
+	float _scale = 4.0f;
+public:
+	void Initialize();
+	void ReLoad();
+	void Update();
+	void Render();
+	void Release();
+	bool IsEndeMotion() { return _animation.IsEndMotion();}
+
+};
+
