2b6937fd43345d019a99128cc0d6b791716c9ba3
b0v0 <w>
2023-01-05 18:20:12 +0900

    テスト

--- Project/DollMopAnimation.cpp
+++ Project/DollMopAnimation.cpp
@@ -38,18 +38,18 @@ void DollMopAnimation::CreateAnimation() {
 	{_wait,8,0},{_wait,9,0} ,{_wait,10,0},{_wait,11,0},{_wait,12,0},{_wait,13,0},{_wait,14,0},{_wait,15,0},{_wait,16,0},
 	{_wait,17,0},{_wait,18,0},{_wait,19,0}}
 },
-	{
+{
 	"Pick_Mop",
 	0,2560,
 	320,320,
 	false,{{_wait,0,0},{_wait,1,0},{_wait,2,0},{_wait,3,0},{_wait,4,0}}
-	},
-	{
+},
+{
 	"Pick_Mop",
 	0,2880,
 	320,320,
 	false,{{_wait,0,0},{_wait,1,0},{_wait,2,0},{_wait,3,0},{_wait,4,0}}
-	},
+},
 	};
 	_motionController.Create(_dollAnimation, _motionCount);
 }
