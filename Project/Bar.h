7952609bbe2b91826a07bf37f00f22f852c04da3
b0v0 <w>
2023-01-16 18:48:45 +0900

    完了

--- Project/Bar.h
+++ Project/Bar.h
@@ -17,9 +17,9 @@ private:
 
 	int _stageValue, _stageNumber;
 	
+	const int _maxDifficulty = 5;
 	int _difficulty;
 	CTexture** _difficultyTextureArray;
-	Vector2 _difficultyPos;
 	float _difficultyScale;
 
 	CTexture* _coinTexture;
@@ -30,21 +30,22 @@ private:
 
 	Vector2 _screenPos, _screenSize;
 
-	bool _clear, _getCoin;	
-	int		interval;
+	bool _clear, _getCoin;
 
 public:
 
 	void Initialize();
 	void ReLoad();
-	void SetStatu(float scale, float space);
 	void SetScreenStatu(Vector2 screenPos, Vector2 screenSize);
 	void SetData(int stageNumber, std::string previewTextureName, std::string barTextureName, int difficulty, std::string stageDataTextName);
 	void SetStageValue(int stageValue) { _stageValue = stageValue; }
 	void SetBaseBarTexture(CTexture* texture) { _baseBarTexture = texture; }
 	void SetCoinTexture(CTexture* texture) { _coinTexture = texture; CalcuCoinScale(); }
+	void SetDifficulutyTexture(CTexture* texture);
 
+	void SetScale(float scale);
 	void Move(float sliderValue);
+
 	void Clear() { _clear = true; }
 	void GetCoin() { _getCoin = true; }
 
@@ -52,19 +53,19 @@ public:
 	void Release();
 
 	bool CheckOnMouse(Vector2 mousePos) { return _barHitBox.CollisionPoint(mousePos); }
-	CTexture* GetPreviewTexture() { return &_previewTexture; }
-	std::string GetStageDataTextName() { return _stageDataTextName; }
 	bool IsClear() { return _clear; }
 	bool IsGetCoin() { return _getCoin; }
-	void SetDifficulutyTexture(CTexture* texture);
 
-	void ScaleUp(float scale) { _scale = scale; }
-	void DifficultScale(float difficltscale, int _intarval) {
-		_difficultyScale = (_barHitBox.GetHeight() / 2 / _difficultyTextureArray[0]->GetHeight() - 0.1f) * difficltscale;   interval = _intarval;}
+	CTexture* GetPreviewTexture() { return &_previewTexture; }
+	std::string GetStageDataTextName() { return _stageDataTextName; }
+
 private:
 	bool CheckOnScreenTopLine(float top,float bottom);
-	CRectangle GetRenderRect(Vector2, CTexture*,float scale);
+	CRectangle GetRenderRect(Vector2 pos, CRectangle rect, float scale);
 	bool IsRenderRange(CTexture*, Vector2, float scale);
 	void CalcuCoinScale();
-	
+	void CalcuDifficultyScale();
+
+	void RenderDifficulty();
+	void RenderCoin();
 };
