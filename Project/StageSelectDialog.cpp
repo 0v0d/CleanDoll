ce3839de465376f21c912a6424e32ceb4b2d4e6d
81905 <81905@LAPTOP-J9VJSPVO>
2023-01-20 16:38:53 +0900

    ;をつけた

--- Project/StageSelectDialog.cpp
+++ Project/StageSelectDialog.cpp
@@ -6,7 +6,7 @@ void StageSelectDialog::Initialize() {
 	const float spaceX = 100;
 	const int spaceY = 60;
 	LoadTexture();
-	LaodSound():
+	LaodSound();
 	CreateButton(&_yesButton, Vector2(_basePos.x - spaceX / 2 - _yesTexture.GetWidth() / 2, _basePos.y + spaceY), &_yesTexture, [&]() {_loadStageMethod(_pickStageNumber); });
 	CreateButton(&_noButton, Vector2(_basePos.x + spaceX / 2 + _noTexture.GetWidth() / 2, _basePos.y + spaceY), &_noTexture, [&]() {
 		_openStaSeleDialog = false;
